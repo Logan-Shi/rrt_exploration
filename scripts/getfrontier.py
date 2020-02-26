3dc5c1c1e7510682cf71a09bd0c29e74d25c3695
Logan-Shi <837898231@qq.com>
2020-02-26 21:16:36 +0800

    submaps

--- scripts/getfrontier.py
+++ scripts/getfrontier.py
@@ -5,6 +5,7 @@
 from copy import copy
 import rospy
 from nav_msgs.msg import OccupancyGrid
+from functions import robot
 
 import numpy as np
 import cv2
@@ -12,27 +13,67 @@ import cv2
 #-----------------------------------------------------
 
 def getfrontier(mapData):
+
+	laserRange = 10
+	robots=[]
+	robots.append(robot("/robot_1"))
+	x,y = robots[0].getPosition()
 	data=mapData.data
 	w=mapData.info.width
 	h=mapData.info.height
 	resolution=mapData.info.resolution
+	r = int(laserRange / resolution)+1
 	Xstartx=mapData.info.origin.position.x
 	Xstarty=mapData.info.origin.position.y
 	#rospy.loginfo("x: "+str(Xstartx)+" y: "+str(Xstarty))
-	 
-	img = np.zeros((h, w, 1), np.uint8)
+	# print("robot at : "+str(x)+str(y))
+	# print("map : "+str(w)+str(h))
+	
+	xRobot = int((x - Xstartx)/resolution)
+	yRobot = int((y - Xstarty)/resolution)
+	# print("Robot index in map : "+str(xRobot)+str(yRobot))
+	# print("x1,y1: "+str([xRobot-r,yRobot-r])+"x2,y2: "+str([xRobot+r,yRobot+r]))
+	rimg = np.zeros((h, w, 1), np.uint8)
+	threshold = 50
 	
 	for i in range(0,h):
 		for j in range(0,w):
-			if data[i*w+j]==100:# walls
-				img[i,j]=0
+			if data[i*w+j]>threshold:# walls
+				rimg[i,j]=0
 			elif data[i*w+j]==-1:# unexplored
-				img[i,j]=205
-			else:
-				img[i,j]=255
+				rimg[i,j]=205
+			else: # free space
+				rimg[i,j]=255
+	x1 = xRobot-r
+	if x1 < 0:
+		x1 = 0
+	y1 = yRobot-r
+	if y1 < 0:
+		y1 = 0
+	x2 = xRobot+r
+	if x2 > w:
+		x2 = w
+	y2 = yRobot+r
+	if y2 > h:
+		y2 = h
+	img = rimg[y1:y2, x1:x2]
+	# for i in range(0,2*r):
+	# 	for j in range(0,2*r):
+	# 		i_d = xRobot-r+i
+	# 		j_d = yRobot-r+j
+	# 		index_d = i_d*w+j_d 
+	# 		if  index_d > len(data) or index_d < 0:
+	# 			img[i,j]=205
+	# 			continue
+	# 		if data[index_d]>threshold:# walls
+	# 			img[i,j]=0
+	# 		elif data[index_d]==-1:# unexplored
+	# 			img[i,j]=205
+	# 		else: # free space
+	# 			img[i,j]=255
 
 	o=cv2.inRange(img,0,1)
-	#cv2.imshow('img',img)
+	# cv2.imshow('img',img)
 
 	kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
 	img1=cv2.dilate(img,kernel)
@@ -47,7 +88,7 @@ def getfrontier(mapData):
 
 	kernel1= cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
 	o1=cv2.erode(o,kernel1)
-	#cv2.imshow('o1',o1)
+	# cv2.imshow('o1',o1)
 
 	res = cv2.bitwise_and(o1,edges)
 	res1=cv2.resize(res,(0,0),fx=0.2,fy=0.2,interpolation=cv2.INTER_NEAREST)
@@ -61,9 +102,9 @@ def getfrontier(mapData):
 	im2, contours, hierarchy = cv2.findContours(frontier,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 	all_pts=[]
 	
-	#cv2.imshow('frontier',frontier)
-	#cv2.waitKey(0)
-	#cv2.destroyAllWindows()
+	# cv2.imshow('frontier',frontier)
+	# cv2.waitKey(0)
+	# cv2.destroyAllWindows()
 
 	if len(contours)>0:
 		upto=len(contours)-1
@@ -75,9 +116,9 @@ def getfrontier(mapData):
 				cnt = contours[i]
 				M = cv2.moments(cnt)
 				#cx = int(M['m10']/M['m00'])
-				cx = 5*int(M['m10']/M['m00'])
+				cx = 5*int(M['m10']/M['m00'])+x1
 				#cy = int(M['m01']/M['m00'])
-				cy = 5*int(M['m01']/M['m00'])
+				cy = 5*int(M['m01']/M['m00'])+y1
 				xr=cx*resolution+Xstartx
 				yr=cy*resolution+Xstarty
 				pt=[np.array([xr,yr])]
